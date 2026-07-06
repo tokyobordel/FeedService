8d287d78d43aeed023ebcd404b10ecd2eadd2b8e
trainee.i.magomedov <ilya.magomedov.90@mail.ru>
2026-07-06 18:26:34 +0400

    Убран cors
    Change-Id: I35dddf721194ee0f1a3435092b47551a7aa125d6

--- feed_service/backend/internal/app/create.go
+++ feed_service/backend/internal/app/create.go
@@ -5,8 +5,6 @@
 package app
 
 import (
-	"traineesheep/feedservice/internal/utils"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -15,18 +13,15 @@ import (
 //
 // Устанавливает ограничение на размер тела запроса (10 МБ, что позволяет
 // загружать до трёх изображений по 2 МБ каждое) и добавляет middleware CORS.
-// Разрешённые источники CORS берутся из переменной окружения FRONTEND_ALLOWED_URLS.
 // Поддержка учётных данных (credentials) включена.
 func New() *fiber.App {
 	app := fiber.New(fiber.Config{
 		BodyLimit: 10 * 1024 * 1024, // ограничение - 10 мб (3 картинки по 2 мб)
 	})
 
-	allowedUrls := utils.GetEnv("FRONTEND_ALLOWED_URLS", "http://localhost:3000, http://10.64.11.142:3000")
-
 	// Настройка cors
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     allowedUrls,
+		AllowOrigins:     "http://localhost:3000",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
 		AllowCredentials: true,
